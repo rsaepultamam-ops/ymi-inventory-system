b5ba5cc51241375cf3ecde89363960f7029b642b
rsaepultamam-ops <rsaepultamam@gmail.com>
2026-06-07 15:22:13 +0700

    Add Tailwind configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2a5298',
+        secondary: '#1e3c72',
+      },
+    },
+  },
+  plugins: [],
+};
